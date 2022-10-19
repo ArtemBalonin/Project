29a345d48da2dc0dec1f646ff64f2eb7600a9590
Artem <a.balonin.52@mail.ru>
2022-10-19 14:06:58 +0300

    Решили задачу №2

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и повторяет, 
+// является ли первое число квадратом второго
+// Например:
+// а = 25, b = 5 -> да
+// а = 2, b = 10 -> нет
+
+Console.Write("Введите первое целое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе целое число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+bool res = num1 == num2 * num2;
+if(res)
+{
+
+}
+Console.Write(res);
