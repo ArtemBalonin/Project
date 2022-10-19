975a4a552b19b6c5e8393a574d6b15514d833f54
Artem <a.balonin.52@mail.ru>
2022-10-19 15:00:03 +0300

    Решили задачу №2

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// Пример:
+// 3 -> среда
+// 5 -> пятница
+
+Console.Write("Введите целое число от 1 до 7: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num == 1) Console.Write("Понедельник");
+if (num == 2) Console.Write("Вторник");
+if (num == 3) Console.Write("Среда");
+if (num == 4) Console.Write("Четверг");
+if (num == 5) Console.Write("Пятница");
+if (num == 6) Console.Write("Суббота");
+if (num == 7) Console.Write("Воскресенье");
+if (num > 7 || num < 1) Console.Write("Такого дня недели не существует");
