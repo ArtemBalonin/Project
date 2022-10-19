8cbfbcbe99ac9cb6b1a2e6108fa17e3de9c6a55f
Artem <a.balonin.52@mail.ru>
2022-10-19 15:42:06 +0300

    Решили задачу №4

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе
+// показывает последнюю цифру этого числа.
+// Пример
+// 456 -> 6
+
+Console.WriteLine("Введите 3-ех значное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(num % 10);
