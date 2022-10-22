c542aa553a274cb10638ae65dbc0e3f453d5dfd5
Artem <a.balonin.52@mail.ru>
2022-10-22 20:54:46 +0300

    Решили задачу №7 используя 3 способа

--- Task07/Program.cs
+++ Task07/Program.cs
@@ -4,15 +4,46 @@
 // Пример: 34, 5 -> не кратно, остаток 4
 // 16, 4 -> кратно
 
+// Console.Write("Введите первое число: ");
+// int firstDigit = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите второе число: ");
+// int secondDigit = Convert.ToInt32(Console.ReadLine());
+
+// if ( firstDigit % secondDigit == 0) Console.Write("Кратно");
+// else
+// {
+//     int num = firstDigit % secondDigit;
+//     Console.Write($"не кратно, остаток {num}");
+// }
+
+// Решение задачи с помощью функции.
+
+// Console.Write("Введите первое число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите второе число: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// bool Multiple(int number1, int number2)
+// {
+//     return number1 % number2 == 0;
+// }
+// bool multiple = Multiple(num1, num2);
+
+// Console.Write(multiple? "кратно" : $"не кратно, остаток {num1 % num2}");
+
+// Решение задачи с помощью оптимизированной функции.
+
 Console.Write("Введите первое число: ");
-int firstDigit = Convert.ToInt32(Console.ReadLine());
+int num1 = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Введите второе число: ");
-int secondDigit = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
 
-if ( firstDigit % secondDigit == 0) Console.Write("Кратно");
-else
+int Multiple(int number1, int number2)
 {
-    int num = firstDigit % secondDigit;
-    Console.Write($"не кратно, остаток {num}");
+    return number1 % number2;
 }
+int rest = Multiple(num1, num2);
+Console.Write(rest == 0 ? "Кратно" : $"Не кратно, остаток {rest}");
