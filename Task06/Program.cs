6e338b868a623bad79a974250f75d31ed4f32435
Artem <a.balonin.52@mail.ru>
2022-10-22 17:23:22 +0300

    Добавили решение задачи с помощью функции

--- Task06/Program.cs
+++ Task06/Program.cs
@@ -1,11 +1,25 @@
 ﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
 // Например: 456 -> 46
+// int num = new Random().Next(100, 1000);
+// Console.WriteLine($"Случайное число из диапозона 100 - 1000 = {num}");
+
+// int firstDigit = num / 100;
+// Console.WriteLine($"Первая цифра числа {num} = {firstDigit}");
+
+// int secondDigit = num % 10;
+// Console.WriteLine($"Третья цифра числа {num} = {secondDigit}");
+// Console.Write($"Итоговое число = {firstDigit}{secondDigit}");
+
+
+// Решение задачи с помощью создания функции.
 int num = new Random().Next(100, 1000);
 Console.WriteLine($"Случайное число из диапозона 100 - 1000 = {num}");
 
-int firstDigit = num / 100;
-Console.WriteLine($"Первая цифра числа {num} = {firstDigit}");
-
-int secondDigit = num % 10;
-Console.WriteLine($"Третья цифра числа {num} = {secondDigit}");
-Console.Write($"Итоговое число = {firstDigit}{secondDigit}");
+int FirstSecondNumber(int number)
+{
+    int firstDigit = number / 100;
+    int secondDigit = number % 10;
+    int result = firstDigit * 10 + secondDigit;
+    return result;
+}
+Console.Write($"Итоговое число = {FirstSecondNumber(num)}");
