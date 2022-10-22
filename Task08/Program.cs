82bd58cba8a3e1659b2138923fa9f8fb870c53b5
Artem <a.balonin.52@mail.ru>
2022-10-22 21:35:32 +0300

    Решили задачу №8 двумя способами

---
+++ Task08/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+// Пример: 
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+// Первый вариант решения
+
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if (num % 7 == 0 && num % 23 == 0)
+// { 
+//     Console.Write("да");
+// }
+
+// else 
+// {
+//     Console.Write("нет");
+// }
+
+// Второй вариант решения с помощью функции.
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string YesNo(int num)
+{
+    return num % 7 == 0 && num % 23 == 0 ? "да" : "нет";
+}
+Console.Write(YesNo(num));
