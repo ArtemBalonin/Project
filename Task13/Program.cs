ad39df0c3abea7e6c6aedadde9b87c435b0e69ec
Artem <a.balonin.52@mail.ru>
2022-10-27 12:44:35 +0300

    Решили задачу №13

---
+++ Task13/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+// и выдает таблицу квадратов чисел от 1 до (N)
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1, 4
+
+void Table(int n)
+{
+    if (n > 0)
+    {
+        int index = 1;
+        while (index <=n)
+        {
+            Console.WriteLine($"{index,4}    {Math.Pow(index, 2)}");
+            index++;
+        }
+    }
+    else Console.WriteLine("Некорректное число");
+}
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Table(n);
