29cf101e81a6daab5e4b8d4f65479702ad77bf82
Artem <a.balonin.52@mail.ru>
2022-11-11 16:55:12 +0300

    Решили задачу №16

---
+++ Task16/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+// и выдает произведение чисел от 1 до N
+// 4 -> 24
+// 5 -> 120
+
+Console.Write("Введите натуральное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int Composition(int num)
+{
+    int sum = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        sum *= i;
+    }
+
+    return sum;
+}
+int composition = Composition(a);
+Console.Write($"Произведение чисел от 1 до {a} = {composition}");
