548ba8f29017ffe58eb48ecfe990382b325ad7a4
Artem <a.balonin.52@mail.ru>
2022-11-11 18:29:57 +0300

    Добавили еще одно решение

--- Task17/Program.cs
+++ Task17/Program.cs
@@ -2,25 +2,54 @@
 // заполняя нулями и единицами в случайном порядке.
 // [1,0,1,1,0,1,0,0]
 
-void FillArray(int[] arr)
+// void FillArray(int[] arr)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         arr[i] = new Random().Next(0,2);
+//     }
+// }
+
+// void PrintArray(int[] arr)
+// {
+//     Console.Write("[");
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         if (i != arr.Length -1 ) Console.Write($"{arr[i]}, ");
+//         else Console.Write($"{arr[i]}");
+//     }
+//     Console.WriteLine("]");
+// }
+
+// int[] array = new int [8];
+// FillArray(array);
+// PrintArray(array);
+
+// Добавляем универсальное решение
+
+int[] CreateArray(int len)
 {
-    for (int i = 0; i < arr.Length; i++)
+    int[] arr = new int[len];
+    for (int i = 0; i < len; i++)
     {
         arr[i] = new Random().Next(0,2);
+    
     }
+    return arr;
 }
 
-void PrintArray(int[] arr)
+void PrintArray (int[] arr)
 {
     Console.Write("[");
     for (int i = 0; i < arr.Length; i++)
     {
-        if (i != arr.Length -1 ) Console.Write($"{arr[i]}, ");
-        else Console.Write($"{arr[i]}");
+        Console.Write(arr[i]);
+        if (i < arr.Length -1) Console.Write(",");
     }
     Console.WriteLine("]");
-}
 
-int[] array = new int [8];
-FillArray(array);
+}
+Console.Write("Введите колличество элементов: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArray(length);
 PrintArray(array);
