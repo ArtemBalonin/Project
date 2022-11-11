7f8c9d8c9ae8a4dcee1129efeb7b92295559bfc9
Artem <a.balonin.52@mail.ru>
2022-11-11 15:59:41 +0300

    Решили задачу №14 2-мя способами с помощью циклов for и whille

---
+++ Task15/Program.cs
@@ -0,0 +1,45 @@
+﻿// Напишите программу, которая принимает на вход число и выдает количество цифр в числе.
+// 456 -> 3
+// 56 -> 2
+// 1234567 -> 7
+
+// Решение с помощью цикла For
+
+// Console.Write("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// int NumberDigits(int num)
+// {
+//     int digits = 0;
+//     for (int i = 0; i < num | i > num;)
+//     {
+//         num /= 10;
+//         digits += 1;
+//     }
+//     return digits;
+// }
+// if (a != 0)
+// {
+//     int num = NumberDigits(a);
+//     Console.Write($"Колличество цифр в числе {a} = {num}");
+// }
+// else Console.Write($"В введеном числе {a}, всего 1 цифра");
+
+// Решение с помощью цикла Wille
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int CountDigits(int num)
+{
+    int count = 0;
+    while (num != 0)
+    {
+        count ++;
+        num /= 10;
+    }
+    return count == 0 ? 1 : count;
+}
+
+int countDigits = CountDigits(a);
+Console.WriteLine($"Колличество цифр в числе {a} = {countDigits}");
