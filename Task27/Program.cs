2ba3f61f77bc4a8928584c0b6911f83575b2d7cc
Artem <a.balonin.52@mail.ru>
2022-11-17 14:37:47 +0300

    Решили задачу №27 копирование массива

---
+++ Task27/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая будет создавать копию заданного массива с помощью
+// поэлементного копирования.
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+int [] arr = CreateArrayRndInt(5, 1, 9);
+PrintArray(arr);
+
+int [] CopyArray(int[] array)
+{
+    int[] copy = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        copy[i] = array[i];
+    }
+    return copy;
+}
+int[] copyArray = CopyArray(arr);
+PrintArray(copyArray);
