cfffb523e6fead7df9ae3df74f466d71e0cf12f6
Artem <a.balonin.52@mail.ru>
2022-11-17 12:54:26 +0300

    Решили задачу №25 использу переворот  Str, Int. While

---
+++ Task25/Program.cs
@@ -0,0 +1,44 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// // 1 решение через int.
+
+// int DecToBin(int number)
+// {
+//     int binNumber = 0;
+//     int d10 = 1;
+//     while (number > 0)
+//     {
+//         binNumber = binNumber + number % 2 * d10;
+//         number /= 2;
+//         d10 *= 10;
+//     }
+//     return binNumber;
+// }
+
+// int decToBin = DecToBin(num);
+// Console.WriteLine($"Число {num} в двоичной системе = {decToBin}");
+
+// 2 решение с помощью string.
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string ConverterDigit(int num)
+{
+    string res = "";
+    while (num > 0)
+    {
+        res = $"{(num % 2)}{res}";
+        num /= 2;
+    }
+    return res;
+}
+
+string converterDigit = ConverterDigit(num);
+Console.WriteLine($"Число {num} в двоичной системе = {converterDigit}");
