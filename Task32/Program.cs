48a65d474dacc2e4d6d5d2180e918852d48e6df0
Artem <a.balonin.52@mail.ru>
2022-11-25 14:04:17 +0300

    Решили задачу №32, Поменяли местами в двумерном массиве 1-ую и последнюю строку

---
+++ Task32/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+int[,] CreateMatrixRndInt(int rows, int colums, int min, int max)
+{
+    int[,] matrix = new int[rows, colums];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5} |");
+            else Console.WriteLine($"{matrix[i, j],5} |");
+        }
+    }
+}
+
+void SwapsPlacesFirstLastLineMatrix(int[,] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        int temp = matrix[0, j];
+        matrix[0, j] = matrix[matrix.GetLength(0) - 1, j];
+        matrix[matrix.GetLength(0) - 1, j] = temp;
+    }
+}
+
+Console.Write("Введите колличество строк массива: m= ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите колличество столбцов массива: m= ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = CreateMatrixRndInt(m, n, -10, 10);
+PrintMatrix(matrix);
+Console.WriteLine();
+SwapsPlacesFirstLastLineMatrix(matrix);
+PrintMatrix(matrix);
