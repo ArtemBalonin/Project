e433974576e3d3321fcc38917a81467a277610a7
Artem <a.balonin.52@mail.ru>
2022-11-25 14:35:54 +0300

    Решили задачу № 33, Заменили строки на столбцы в двумерном массиве

---
+++ Task33/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задайте двумерный массив. 
+// Напишите программу, которая заменяет строки на столбцы.
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+int[,] CreateMatrixRndInt(int rows, int colums, int min, int max)
+{
+    int[,] matrix = new int[rows, colums];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5} |");
+            else Console.WriteLine($"{matrix[i, j],5} |");
+        }
+    }
+}
+
+void ReplaceRowsColums(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0) - 1; i++)
+    {
+        for (int j = i + 1; j < matrix.GetLength(1); j++)
+        {
+            int temp = matrix[i, j];
+            matrix[i, j] = matrix[j, i];
+            matrix[j, i] = temp;
+        }
+    }
+}
+
+Console.Write("Введите колличество строк массива: m= ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите колличество столбцов массива: n= ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = CreateMatrixRndInt(m, n, -10, 10);
+PrintMatrix(matrix);
+Console.WriteLine();
+if(m==n)
+{
+    ReplaceRowsColums(matrix);
+    PrintMatrix(matrix);
+}
+else Console.WriteLine("Массив не квадратный, замена строк и столбцов невозможна.");
