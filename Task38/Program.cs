65a0b2715469a903d91d60dbec0681d998e193eb
Artem <a.balonin.52@mail.ru>
2022-11-28 16:22:04 +0300

    Решили задачу №38, Вернули сумму цифр числа с помощью рекурсии

---
+++ Task38/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр
+// 453 -> 12
+// 45 -> 9
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int SumNumbers(int number)
+{
+    if(number == 0) return 0;
+    return number%10 + SumNumbers(number / 10);
+
+}
+
+int sum = SumNumbers(num);
+Console.Write(sum);
