b5aa9bfdbfbb6e65ba3926624f67f4508ab3057f
Artem <a.balonin.52@mail.ru>
2022-11-28 15:10:34 +0300

    Решили задачу №36 используя рекурсию

---
+++ Task36/Program.cs
@@ -0,0 +1,16 @@
+﻿// задайте значение N. Напишите программу, которая выдает все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+Console.Write("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void NaturalNambers(int num)
+{
+    if(num == 0) return;
+    NaturalNambers(num - 1);
+    Console.Write($"{num} ");
+
+}
+
+NaturalNambers(number);
